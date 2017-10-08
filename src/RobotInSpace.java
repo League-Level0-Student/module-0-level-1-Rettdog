b182cdc6cc35c2d23840245d498fb8f0a89f5371
League Computer <>
2017-10-08 11:30:11 -0700

    RobotInSpace

--- src/RobotInSpace.java
+++ src/RobotInSpace.java
@@ -17,26 +17,39 @@ import org.jointheleague.graphical.robot.Robot;
 	
 	public class RobotInSpace implements KeyEventDispatcher {
 		
-	Robot rob = new Robot("mini");
+	Robot jimmy = new Robot("mini");
 
 	/*  Make the Robot move around the screen when the arrow keys are pressed... */
-	private void moveRobot(int keyPressed) {
+	private void moveRobot(int keyPressed){
 	    // 0. Print out the keyPressed variable and write down the numbers for each arrow key
+System.out.println(keyPressed);
+	    // 1. If the up arrow is pressed, move the Robot up the screen.38
+if(keyPressed==38) {
+	jimmy.setAngle(0);
+	jimmy.move(10);
+}
+	    // 2. If the down arrow is pressed, move the Robot down.40
+if(keyPressed==40) {
+	jimmy.setAngle(180);
+	jimmy.move(10);
 
-	    // 1. If the up arrow is pressed, move the Robot up the screen.
-
-	    // 2. If the down arrow is pressed, move the Robot down.
-
-	    // 3. If the left arrow is pressed, make the Robot go left. Hint: Make sure to end with the Robot facing UP.
-	    
-	    // 4. If right is pressed, move the Robot right.
-	    
+}
+	    // 3. If the left arrow is pressed, make the Robot go left. Hint: Make sure to end with the Robot facing UP.37
+	    if(keyPressed==37) {
+	    	jimmy.setAngle(270);
+	    	jimmy.move(10);
+	    }
+	    // 4. If right is pressed, move the Robot right.39
+	    if(keyPressed==39) {
+	    	jimmy.setAngle(90);
+	    	jimmy.move(10);
+	    }
 	    // 5. Then move the Robot to RD-2D for a surprise! 
 	}
 
 	private void checkIfR2D2Found() throws Exception {
-	    int robotLocationX = rob.getX();
-	    int robotLocationY = rob.getY();
+	    int robotLocationX = jimmy.getX();
+	    int robotLocationY = jimmy.getY();
 
 		if (robotLocationX <= 7300 && robotLocationX >= 720 && robotLocationY >= 150 && robotLocationY <= 160)
 	        playEureka();
@@ -49,9 +62,10 @@ import org.jointheleague.graphical.robot.Robot;
 
 	private void controlTheRobot() {
 	    KeyboardFocusManager.getCurrentKeyboardFocusManager().addKeyEventDispatcher(this);
-	    rob.setWindowImage("planet.jpg");
-	    rob.penUp();
-	    rob.setSpeed(10);
+	    jimmy.setWindowImage("planet.jpg");
+	    
+	    jimmy.penUp();
+	    jimmy.setSpeed(10);
 	}
 
 	public boolean dispatchKeyEvent(KeyEvent e) {
